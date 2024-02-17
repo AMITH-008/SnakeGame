e06c22018aa29d529a54a8ce93e95a6bb88bc1f0
amith <amithkouti@gmail.com>
2024-02-17 16:14:34 +0530

    Added snake component to game panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -64,6 +64,17 @@ public class GamePanel extends JPanel implements ActionListener {
 
         g.setColor(Color.RED);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for(int i=0;i<bodyParts;i++) {
+            if(i==0) {
+                g.setColor(Color.black);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }else {
+                g.setColor(Color.blue);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+
+        }
     }
 
     public void newApple() {
@@ -72,6 +83,26 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void move () {
+
+        for(int i=bodyParts;i>0;i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] -UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+
+        }
 
     }
 
